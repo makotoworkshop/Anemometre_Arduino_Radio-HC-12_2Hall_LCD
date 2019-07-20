9709c78832e251d155fd2a16a05409c452762bc0
makotoworkshop <makoto@debianbox.makotoworkshop.org>
2019-07-20 20:59:04 +0200

    Update Anemometre_Arduino_Radio-HC-12_2Hall_Transmitter.ino

--- Anemometre_Arduino_Radio-HC-12_2Hall_Transmitter.ino
+++ Anemometre_Arduino_Radio-HC-12_2Hall_Transmitter.ino
@@ -18,6 +18,8 @@ float intervalleRPM = 0;
 char MessageVent[] = " VENT\n"; // alarm message to be sent; '\n' is a forced terminator char
 char MessageTest[] = " EOLIENNE\n"; // alarm message to be sent; '\n' is a forced terminator char
 String chaine;
+int rotaAnemo = 0;
+int rotaEol = 0;
 
 /*********/
 /* SETUP */
@@ -25,7 +27,7 @@ String chaine;
 void setup() {
   HC12.begin(9600);               // Serial port to HC12
   // debug uniquement, penser à commenter toutes les lignes «Serial…» pour éviter les erreurs de valeur (calcul trop long pour l'interruption)
-  Serial.begin(115200);             // Serial port to computer
+//  Serial.begin(115200);             // Serial port to computer
   // Pin capteurs
   attachInterrupt(0, rpm_vent, FALLING);  // le capteur à effet Hall Anémomètre est connecté à la pin 2 = int0
   attachInterrupt(1, rpm_eol, FALLING);  // le capteur à effet Hall Éolienne est connecté à la pin 3 = int1      
@@ -35,11 +37,11 @@ void setup() {
 /* PROGRAMME */
 /*************/
 void loop() {
- // RemiseZeroVitVentKMH ();
-//  RemiseZeroVitEolRPM ();
-   
+  RemiseZeroVitVentKMH ();
+  RemiseZeroVitEolRPM ();
+  
   chaine = String(vitVentKMH) + MessageVent + String(vitEolRPM) + MessageTest;
-  Serial.println ( "chaine String : " +chaine );
+//  Serial.println ( "chaine String : " +chaine );
   HC12.print(chaine); 
   
   delay(100);
@@ -59,10 +61,11 @@ void rpm_vent()   // appelée par l'interruption, Anémomètre vitesse du vent.
     rpmVent = 60 / (intervalleKMH /1000);  
   }
   vitVentKMH = ( rpmVent + 6.174 ) / 8.367;
-  Serial.print ( "vitVentKMH : " );
-  Serial.println ( vitVentKMH ); // affiche les rpm  
-  Serial.println ( "" );
+//  Serial.print ( "vitVentKMH : " );
+//  Serial.println ( vitVentKMH ); // affiche les rpm  
+//  Serial.println ( "" );
   dateDernierChangementVent = dateCourante;
+  rotaAnemo = 1;
 }
 
 void rpm_eol()    // appelée par l'interruption, Tachymétre rotation éolienne.
@@ -75,40 +78,49 @@ void rpm_eol()    // appelée par l'interruption, Tachymétre rotation éolienne
   {  
     vitEolRPM = 60 / (intervalleRPM /1000);
   }
-  Serial.print ( "rpm : " );
-  Serial.println ( vitEolRPM ); // affiche les rpm  
-  Serial.println ( "" );
+ // Serial.print ( "rpm : " );
+ // Serial.println ( vitEolRPM ); // affiche les rpm  
+ // Serial.println ( "" );
   dateDernierChangementEol = dateCourante;
+  rotaEol = 1;
 }
-
 
 
 void RemiseZeroVitVentKMH ()
 {
   unsigned long dateCouranteKMH = millis();
-  if (intervalleKMH == intervalleKMH) // Si ça ne tourne plus (valeur plus mise à jour)
+  if (rotaAnemo == 1 ){ // teste si l'anémemètre tourne
+//    Serial.println ( "Anémo tourne ");
+    rotaAnemo = 0;
+  }
+  else    // Si ça ne tourne plus (valeur plus mise à jour)
   {  
     float dureeKMH = (dateCouranteKMH - dateDernierChangementKMH);
     if (dureeKMH > 10000) // Si ça ne tourne plus depuis 10 secondes
     {
-      Serial.print ( "dureeKMH : " );
-      Serial.println ( dureeKMH ); // affiche les rpm  
+//      Serial.print ( "dureeKMH : " );
+//      Serial.println ( dureeKMH ); // affiche les rpm  
       vitVentKMH = 0;  // Remsise à zero !
       dateDernierChangementKMH = dateCouranteKMH;    
     }
   }
 }
 
+
 void RemiseZeroVitEolRPM ()
 {
   unsigned long dateCouranteRPM = millis();
-  if (intervalleRPM == intervalleRPM) // Si ça ne tourne plus (valeur plus mise à jour)
+  if (rotaEol == 1 ){ // teste si l'éolienne tourne
+//    Serial.println ( "Éolienne tourne ");
+    rotaEol = 0;
+  }
+  else    // Si ça ne tourne plus (valeur plus mise à jour)
   {  
     float dureeRPM = (dateCouranteRPM - dateDernierChangementRPM);
     if (dureeRPM > 65000) // Si ça ne tourne plus depuis 65 secondes (soit moins de 1 rpm)
     {
-      Serial.print ( "dureeRPM : " );
-      Serial.println ( dureeRPM ); // affiche les rpm  
+//      Serial.print ( "dureeRPM : " );
+//      Serial.println ( dureeRPM ); // affiche les rpm  
       vitEolRPM = 0;  // Remsise à zero !
       dateDernierChangementRPM = dateCouranteRPM;    
     }
